871400863f901d6f847feafab4049013065d49c7
Johnathan Pruitt <pruittj2000@yahoo.com>
2020-02-18 21:47:49 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,8 +1,14 @@
 def mario
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+  phase = "It's-a me, Mario!"
   puts phrase
 end
 
 def toadstool
+  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
+end
+
+def any_phrase
+  phrase = "Do A Barrel Roll!"
+  puts phrase
 end
